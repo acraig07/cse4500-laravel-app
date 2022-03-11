da5b09be245391e477835c8262ec5d43d5bd6192
RobotNinja07 <57858475+RobotNinja07@users.noreply.github.com>
2022-03-11 12:24:22 -0800

    dsaasd

---
+++ resources/views/calanders/create.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+
+@section('title', 'Calendar')
+
+@section('content_header')
+    <h1>Calendar</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('todos.store') }}" >
+    @csrf
+    <x-adminlte-input name="title" label="Title" />
+    <x-adminlte-input name="start_date" type="date" label="Start Date" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
