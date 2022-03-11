915da32f739f6f854c263af0cdfe98958ceb84bf
RobotNinja07 <57858475+RobotNinja07@users.noreply.github.com>
2022-03-11 12:31:09 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Models\Calendar;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,7 @@ Route::get('/todos', function () {
 });
 
 Route::resource('/todos', TodoController::class);
+Route::resource('/calendars', CalendarController::class);
 Route::get('/calendar', function () {
     return view('calendar');
 });
