a2031ad829dfe9a7eab2427b7558467547fe2b60
RobotNinja07 <57858475+RobotNinja07@users.noreply.github.com>
2022-03-11 12:50:47 -0800

    Update create.blade.php

--- resources/views/calendars/create.blade.php
+++ resources/views/calendars/create.blade.php
@@ -11,6 +11,9 @@
     @csrf
     <x-adminlte-input name="title" label="Title" />
     <x-adminlte-input name="start_date" type="date" label="Start Date" />
+    <x-adminlte-input name="start_time" type="time" label="Start Time" />
+    <x-adminlte-input name="end_date" type="date" label="End Date" />
+    <x-adminlte-input name="end_time" type="time" label="End Time" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
